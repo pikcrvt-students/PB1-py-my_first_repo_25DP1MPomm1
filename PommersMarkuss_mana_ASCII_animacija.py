a188e5289aae0feefa6afd6adf2ff5339e642b2d
25DP1MPomm <A250574MP@rvt.lv>
2026-03-19 09:52:43 +0200

    Sāku veidot savu pirmo ASCII animāciju par cipariem.

---
+++ PommersMarkuss_mana_ASCII_animacija.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+from time import sleep
+
+os.system('clear')
+
+print('0 1 2 3 4 5 6 7 8 9')
+
+sleep(0.5)
+
+os.system('clear')
+
+print('0   2   4   6   8  ')
+print('  1   3   5   7   9')
+
+
+print('  0   2   4   6   8 ')
+print('  1   3   5   7   9')
+
+
+print('    0   2   4   6   8')
+print('  1   3   5   7   9')
+
+
+print('')
+print('  1 0 3 2 5 4 7 6 9 8')
