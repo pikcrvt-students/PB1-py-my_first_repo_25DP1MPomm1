e66ddf805a58a19c0453a5b097618b7ee7274e26
Markuss Jānis Pommers <A250574MP@rvt.lv>
2026-06-08 15:45:47 +0300

    Izveidoju otro programmu no logu veidosanas temas

---
+++ PommersMarkuss_logu_veidosana_2.py
@@ -0,0 +1,12 @@
+from tkinter import *
+def Adarbiba():
+    print('Paldies!')
+def Bdarbiba():
+    print('Au! Man sāp!')
+
+logs = Tk()
+pogaA() = Button(logs, text='Klikšķini te!', command=Adarbiba)
+pogaB() = Button(logs, text='Neklikšķini te!', command=Bdarbiba)
+pogaA.pack()
+pogaB.pack()
+input('Press <ENTER> to continue....')
