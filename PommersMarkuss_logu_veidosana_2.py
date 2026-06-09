9b98b5b768f436872bd75cf2da856e73d501c873
25DP1MPomm <a250574mp@rvt.lv>
2026-06-09 21:20:53 +0300

    Izlaboju 2. loga veidosanas programmu.

--- PommersMarkuss_logu_veidosana_2.py
+++ PommersMarkuss_logu_veidosana_2.py
@@ -5,8 +5,8 @@ def Bdarbiba():
     print('Au! Man sāp!')
 
 logs = Tk()
-pogaA() = Button(logs, text='Klikšķini te!', command=Adarbiba)
-pogaB() = Button(logs, text='Neklikšķini te!', command=Bdarbiba)
+pogaA = Button(logs, text='Klikšķini te!', command=Adarbiba)
+pogaB = Button(logs, text='Neklikšķini te!', command=Bdarbiba)
 pogaA.pack()
 pogaB.pack()
-input('Press <ENTER> to continue....')
+logs.mainloop()
