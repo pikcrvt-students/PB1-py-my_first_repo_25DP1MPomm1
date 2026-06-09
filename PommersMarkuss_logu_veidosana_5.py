455cf197bc6423ddb038c138614c55fc568d2e54
25DP1MPomm <a250574mp@rvt.lv>
2026-06-09 21:43:03 +0300

    Izveidoju 5. programmu, par temu logu veidosana

---
+++ PommersMarkuss_logu_veidosana_5.py
@@ -0,0 +1,12 @@
+from tkinter import *
+logs = Tk()
+logs.title('Citplanētietis')
+a = Canvas(logs, height=300, width=300)
+a.pack()
+kermenis = a.create_oval(100, 150, 300, 250, fill='green')
+acs = a.create_oval(170, 70, 230, 130, fill='white')
+zilite = a.create_oval(190, 90, 210, 110, fill='black')
+mute = a.create_oval(150, 220, 250, 240, fill='red')
+kakls = a.create_line(200, 150, 200, 130)
+cepure = a.create_polygon(180, 75, 220, 75, 200, 20, fill='blue')
+logs.mainloop()
